855a7fe999db54ef8a49917af8a9e4570bcf82f6
joseda1807 <pedrodo221@hotmail.com>
2024-08-29 11:00:02 -0500

    Commit with Js
    Colocamos el Js

---
+++ GeometryDraw/src/js/vectorCalculator.js
@@ -0,0 +1,13 @@
+function vector(){
+    
+    let ax = document.getElementById("puntoAX").value;
+    let ay = document.getElementById("puntoAY").value;
+    let az = document.getElementById("puntoAZ").value;
+
+    let bx= document.getElementById("puntoBX").value;
+    let by= document.getElementById("puntoBY").value;
+    let bz = document.getElementById("puntoBZ").value;
+    alert("("+ ax+ ","+ ay+","+az+")"); 
+    alert("("+ bx+ ","+ by+","+bz+")"); 
+
+}
